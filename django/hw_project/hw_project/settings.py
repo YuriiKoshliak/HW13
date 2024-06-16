9b8f71fbc1c8679b3c3cdcf82973e785a1615eca
Yurii Koshliak <satorxp10@gmail.com>
2024-06-16 18:55:51 +0200

    delete some comments

--- django/hw_project/hw_project/settings.py
+++ django/hw_project/hw_project/settings.py
@@ -143,13 +143,3 @@ LOGIN_URL = "/users/login"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-# EMAIL_HOST = env('EMAIL_HOST')
-# EMAIL_PORT = env('EMAIL_PORT')
-# EMAIL_STARTTLS = False
-# EMAIL_USE_SSL = True
-# EMAIL_USE_TLS = False
-# EMAIL_HOST_USER = env('EMAIL_HOST_USER')
-# EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
-# DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
